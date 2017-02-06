d7d3983213a8182f1733f71eb156142854db2e45
Pradeep kumar jain <pradeep5338.aiesec@gmail.com>
2017-02-06 19:51:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,7 @@ var img=document.getElementById('img');
 var marginleft=0;
 function moveright() {
     marginleft=marginleft+10;
-    img.style.marginleft=marginleft+'px';
+    img.style.marginleft=marginleft + 'px';
 }
 img.onclick = function(){
     var interval = setInterval(moveright,300);
