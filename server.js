27fbbd051cfe6a014dc96d409cb8ec41fe80d808
Pradeep kumar jain <pradeep5338.aiesec@gmail.com>
2017-02-06 17:17:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articlename',function(req,res){
-    var articlename = req.param.articlename;
+    var articlename = req.params.articlename;
   res.send(createtemplate(articles(articlename)));
 });
 
