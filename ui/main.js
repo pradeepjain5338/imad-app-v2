3d76a808b62411eaab3f6fdee49cb6767aa7e8c4
Pradeep kumar jain <pradeep5338.aiesec@gmail.com>
2017-02-10 08:18:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,28 @@
-var button =document.getElementById("counter")
-var counter=0;
+var button =document.getElementById("counter");
+
 
 button.onclick= function() {
-    counter =counter + 1;
+    
+    var request = new XMLhttpRequest() ;
+    
+    request.onreadystatechange = function() {
+        
+        if(request.readystate === XMLhttprequest.DONE)
+{
+    if(request.status === 200)
+    {
+        var counter= request.responseText;
+     counter =counter + 1;
     var span =document.getElementById("count");
-    span.innerHTML=counter.toString();
+    span.innerHTML=counter.toString();   
+    }
+}        
+        
+    };
+    
+  request.open('GET','http://http://pradeepjain5338.imad.hasura-app.io/counter',true)  ;
+  request.send(null);
+    
+    
+    
 };
