49e0b69101ce2328af63f464d59aa812fc6603b3
Pradeep kumar jain <pradeep5338.aiesec@gmail.com>
2017-02-10 08:33:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ var button =document.getElementById("counter");
 
 button.onclick= function() {
     
-    var request = new XMLhttpRequest() ;
+    var request = new XMLHttpRequest() ;
     
     request.onreadystatechange = function() {
         
