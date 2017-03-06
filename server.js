0d25354f4ebac0a335fcb4b088ba24ac5e466ec6
Pradeep kumar jain <pradeep5338.aiesec@gmail.com>
2017-03-06 02:41:39 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.get('/test-db',function(req,res){
    }
 })});
 
-app.get('article/:articl1',function(req,res){
+app.get('article/:article1',function(req,res){
     
     pool.query("select * from article where title="+artilce1,function(err,result)
     {
