624ab826a8c5a0c02bddfe03867d727e7736f9ba
Pradeep kumar jain <pradeep5338.aiesec@gmail.com>
2017-03-14 22:27:10 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -157,14 +157,24 @@ app.post('/login',function(req,res)
   var password=req.body.password;
   var salt=crypto.randomBytes(128).toString('hex');
   var dbstring=hash(password,salt);  
-  pool.query('SELECT * FROM "user" WHERE "username"=$1',[username],function(err,result)
+  pool.query('SELECT * FROM "user" WHERE username=$1',[username],function(err,result)
   {
   if(err){
-   res.status(500).send(err.toString());
+   res.send(500).send(err.toString());
    }
    else if(result.rows.length=== 0)
    {
+     res.send(403).send('username/password is invalid');
+   }else {
       var dbstring=result.rows[0].password;
+      var salt=dbstring.split('$')[3];
+      var hashedpassword=hash(password,salt);
+      
+      if(hashedpassword===dbdtring){
+          req.send('user successfully logged');}
+          else{
+          req.send(403).send('username/password is invalid');
+      }
    }
 });
 });
