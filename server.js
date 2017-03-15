b8f5a42fa36578824fdc66977d4be8085ce25687
Pradeep kumar jain <pradeep5338.aiesec@gmail.com>
2017-03-15 11:56:47 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.post('/login',function(req,res)
   pool.query('SELECT * FROM "user" WHERE username=$1',[username],function(err,result)
   {
   if(err){
-   res.send(500).send(err.toString());
+   res.status(500).send(err.toString());
    }
    else if(result.rows.length=== 0)
    {
