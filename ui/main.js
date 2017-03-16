7516266b60817979233a04dadcfa8bd5a99b6adb
Pradeep kumar jain <pradeep5338.aiesec@gmail.com>
2017-03-16 12:46:19 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-var button =document.getElementById("counter");
+/*var button =document.getElementById("counter");
 
 
 button.onclick= function() {
@@ -25,6 +25,9 @@ button.onclick= function() {
 };
 var nameinput= document.getElementById("name");
 
+//submit username/password to login
+
+var submit =document.getElementById("sub_bttn");
 submit.onclick =function() {
     var request = new XMLHttpRequest() ;
     
@@ -47,10 +50,43 @@ submit.onclick =function() {
 }        
         
     } ;
-    var names = nameinput.value;
-var submit =document.getElementById("submit");
-  request.open('GET','http://pradeepjain5338.imad.hasura-app.io/submit-names?names='+names,true)  ;
-  request.send(null);
+ 
+ /*   var names = nameinput.value;
+;*/
+
+//submit username/password to login
+
+var submit =document.getElementById("sub_bttn");
+submit.onclick =function() {
+    var request = new XMLHttpRequest() ;
+    
+    request.onreadystatechange = function() {
+        
+        if(request.readyState === XMLHttpRequest.DONE)
+{
+    if(request.status === 200)
+    {
+     console.log('user logged in');
+     alert('login success');
+    
+    }
+    else if(request.status === 403){
+       alert('credential invalid'); 
+    }
+    else if(request.status === 500){
+       alert('something went wrong on the server'); 
+    }
+}        
+        
+    } ;
+ 
+var username =document.getElementById('username').value;
+var password =document.getElementById('password').value;
+console.log(username);
+console.log(password);
+  request.open('POST','http://pradeepjain5338.imad.hasura-app.io/login',true)  ;
+  request.setRequestHeader('content-type','application/json');
+  request.send(JSON.strigify({username : username,password : password}));
     
 
     
