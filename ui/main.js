2c64a2f4b1d4a4b1162506f97081229636fedbf0
Pradeep kumar jain <pradeep5338.aiesec@gmail.com>
2017-03-16 13:17:47 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -85,7 +85,7 @@ console.log(username);
 console.log(password);
   request.open('POST','http://pradeepjain5338.imad.hasura-app.io/login',true)  ;
   request.setRequestHeader('Content-Type','application/json');
-  request.send(JSON.strigify({username : username,password : password}));
+  request.send(JSON.stringify({username : username,password : password}));
     
 
     
